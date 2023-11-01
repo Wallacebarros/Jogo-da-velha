d973911ffb6a82301c715508a4e0b48ee6d9fbf8
Wallace <w.wallacebarros.cruz@gmail.com>
2023-10-31 22:20:29 -0300

    header versão 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link da documentação
-        </a>
+    <div>
+      <header>
+        <a href=' ' className="link-sobre">sobre</a>
       </header>
     </div>
   );
